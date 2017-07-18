76306d2bc67cf4991adb94e8fe10bb4dac3a6ff5
Mensah Attisso <mattisso@atd-us.com>
2017-07-18 17:12:51 -0400

    added webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import path  from 'path';
 import open from 'open';
+import  webpack from 'webpack';
+import config from '../webpack.config.dev';
 const  port=5000;
 const  app=express();
+const compiler = webpack(config);
 
-/* const compiler= webpack(config);
 app.use(require('webpack-dev-middleware')(compiler,{
   noInfo:true,
-  publicpath: config.output.publicpath
-})); */
+  publicPath: config.output.publicPath
+}));
 app.get('/', function(req,res){
 
   res.sendfile(path.join(__dirname,'../src/index.html'));
