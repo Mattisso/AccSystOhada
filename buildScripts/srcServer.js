cc94e029c138ddb27e35a95ded9ea7f0047ed708
Mensah Attisso <mattisso@atd-us.com>
2017-07-18 18:37:18 -0400

    remove noInfo & debug from webpack.config.dev

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,7 +8,7 @@ const  app=express();
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler,{
-  noInfo:true,
+  watch:true,
   publicPath: config.output.publicPath
 }));
 app.get('/', function(req,res){
