5c9d6f0b967eb964d7c1e6afd986ef456c8c3907
Mensah A <attmensah@gmail.com>
2017-07-19 22:17:46 -0400

    conflit resolution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import './index.css';
-
-import numeral  from 'numeral';
-const courseValue=numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for thus awesome course!`);
-=======
 import  './index.css';
 import numeral  from 'numeral';
 const courseValue=numeral(1000).format('$0,0.00');
 //debugger;
 console.log(`I would pay ${courseValue} for thus awesome course!`);// eslint-disable-line no-console
 
->>>>>>> daf0c9288b6445f15fe886aaa1b149afa3a724e6
