c49b19d1c33da62693069028a37317d8726c85f7
Mensah A <attmensah@gmail.com>
2017-07-23 14:55:58 -0400

    update  index.test

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
-//import fs  from 'fs';
+import fs  from 'fs';
 const {JSDOM} = jsdom;
 
 
@@ -10,24 +10,13 @@ describe('Our first test', () => {
     });
 });
 
-
 describe('index.html', () => {
-  it('should say hello', () => {
-const virtualConsole = new jsdom.VirtualConsole();
-const dom = new JSDOM(`./src/index.html`, { virtualConsole });
-virtualConsole.on("error", (done) => { 
+  it('should say hello', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+const dom = new JSDOM(index).window;
   const h1 = dom.window.document.getElementsByTagName('h1')[0];
- expect(h1.innerHTML).to.equal("Hellow World!");
+ expect(h1.innerHTML).to.equal("Hello World!");
     done();
       dom.window.close();
-});
-    /* const index = fs.readFileSync('./src/index.html', "utf-8");
-
-    jsdom.VirtualConsole(index, function(err, window) {
-      const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hellow World!");
-     done();
-      window.close();
-    }); */
   });
 });
