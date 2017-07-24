053316f7500304120f602ac8405e8b4cfb630a32
Mensah A <attmensah@gmail.com>
2017-07-23 20:10:27 -0400

    complateModule10

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -5,10 +5,24 @@ const baseUrl =getBaseUrl();
 export function getUsers() {
   return get('users');
 }
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
 
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
 }
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
+}
+
+
 
 function onSuccess(response) {
   return response.json();
