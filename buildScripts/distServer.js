2d2b593ea12a9a5f0a5d2c0be16a18316f2e6b19
Mensah A <attmensah@gmail.com>
2017-07-29 17:45:12 -0400

    added css

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -13,7 +13,7 @@ app.use(express.static('dist'));
 
 app.get('/', function(req,res){
 
-  res.sendfile(path.join(__dirname,'../dist/index.html'));
+  res.sendFile(path.join(__dirname,'../dist/index.html'));
 });
 
 app.listen(port, function(err){
